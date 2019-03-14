efb4d19823a7749becb3306b51b1dbb89dca6317
Modestas <odekst@gmail.com>
2019-03-14 23:51:51 +0100

    Styling

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -4,7 +4,7 @@ const Card = ({robotName, robotId, robotUser}) => {
 	return (
 		<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 tc shadow-5'>
 			<img alt ='image' src={`https://robohash.org/${robotId}?200x200`}/>
-			<h1>{robotName}</h1>
+			<h2>{robotName}</h2>
 			<p>{robotUser}</p>
 		</div>
 	);
